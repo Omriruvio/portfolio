e0736e7d192406a262a0885aa9cb3f3d1698843f
Omri Ruvio <omriruvio@gmail.com>
2022-07-16 21:36:01 +0300

    fix: separate news-explorer from main site

--- news-explorer/src/components/ArticleSection/ArticleSection.js
+++
@@ -1,7 +0,0 @@
-import './ArticleSection.css';
-
-const ArticleSection = ({ children }) => {
-  return <section className='results'>{children}</section>;
-};
-
-export default ArticleSection;
--- news-explorer/src/components/ProtectedRoute/ProtectedRoute.js
+++
@@ -1,7 +0,0 @@
-import { Navigate } from 'react-router-dom';
-
-const ProtectedRoute = ({ isLoggedIn, redirectPath, children, ...props }) => {
-  return isLoggedIn ? children : <Navigate {...props} to={redirectPath} />;
-};
-
-export default ProtectedRoute;
